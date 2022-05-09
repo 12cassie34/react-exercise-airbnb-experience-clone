f22662abc23fd9a66630b87c0070a0a2633abdb6
Shan Yu Lin <ineslin35@gmail.com>
2022-05-09 20:55:50 +0800

    feat: invole .card-section in the main content's layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import './App.css'
-import TheHeader from './components/Header'
-import HeroSection from './components/HeroSection'
-import TheCard from './components/Card';
+import "./App.css";
+import TheHeader from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import TheCard from "./components/Card";
 
-import courseData from "./data"
+import courseData from "./data";
 
 function App() {
-  const cards = courseData.map(course => 
-    <TheCard key={course.id}
-             item={course} />
-  )
+  const cards = courseData.map((course) => (
+    <TheCard key={course.id} item={course} />
+  ));
   return (
     <div className="App">
       <TheHeader />
       <div className="content__container">
         <HeroSection />
+        <div className="card-section">{cards}</div>
       </div>
-      <section className="card-section">
-        {cards}
-      </section>
     </div>
   );
 }
